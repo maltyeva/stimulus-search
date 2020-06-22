adee09d1c64e3e718ee1eacd710bb386a688b55b
Maria <maria.altyeva@gmail.com>
2020-06-22 17:12:57 +0800

    updated search controller

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,5 +1,8 @@
 import { Controller } from 'stimulus';
 
+import Rails from "@rails/ujs";
+
+
 export default class extends Controller {
 	static targets = ["query", "cocktails"]
 
@@ -8,25 +11,13 @@ export default class extends Controller {
 
   submit() {
     const value = this.queryTarget.value
-    fetch(`/?query=${value}`, {
-      headers: { accept: 'application/json'}
-    }).then((response) => response.json())
-    .then(data => { 
-     var cocktailHTML = "";
-     var cocktailArray = Object.values(data)[0]
-     cocktailArray.forEach(cocktail => {
-      cocktailHTML += this.cocktailTemplate(cocktail)
-    });
-     this.cocktailsTarget.innerHTML = cocktailHTML;
-   });  
-  }
-
-
-  cocktailTemplate(cocktail) {
-    return `<div>
-    <h4>${cocktail.name} <small>${cocktail.glass}</small></h4>
-    <p>${cocktail.preparation}</p>
-    </div> `
+    Rails.ajax({
+      type: "GET",
+      url: `/?query=${value}`,
+      success: (_data, _status, xhr) => {
+        this.element.outerHTML = xhr.response
+      }
+    })
   }
 
 }
